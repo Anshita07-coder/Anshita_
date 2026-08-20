e24dbfe8d470f677cec163a82446e7d212dad0f2
Anshita07-coder <anshitagarge@gmail.com>
2026-08-20 20:27:50 +0530

    Added variable

--- anshi.cpp
+++ anshi.cpp
@@ -2,6 +2,14 @@
 using namespace std;
  
 int main() {
-    cout<<"Hello";
+    int quant;
+    int choice;
+    //quanitity
+    int Qrooms = 0 , Qpasta = 0 , Qnoodles = 0 , Qburger = 0 , Qroti = 0 , Qsabji = 0;
+    //food items sold
+    int Srooms = 0 , Spasta = 0 , Snoodles = 0 , Sburger = 0 , Sroti = 0 , Ssabji = 0;
+    //Total proce of items 
+    int Total_rooms = 0 , Total_pasta = 0 , Total_noodles = 0 , Total_burger = 0 , Total_roti = 0 , Total_sabji = 0;
+    
     return 0;
 }
